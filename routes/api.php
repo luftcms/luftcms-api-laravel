8323e9262a1d51d0515dd52f3db2f0a1483fe261
Nawwar Elnarsh <sirnarsh@gmail.com>
2020-03-22 01:55:56 +0100

    Add /ping endpoint

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,12 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+
+Route::get('ping', function () {
+    // Health check ping
+    return ['message' => 'pong'];
+});
+
 Route::middleware('auth:api')->group(function () {
    Route::apiResource('users', 'UserController');
    Route::apiResource('items', 'ItemController');
--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -14,7 +14,7 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        $response = $this->get('/');
+        $response = $this->get('/ping');
 
         $response->assertStatus(200);
     }
